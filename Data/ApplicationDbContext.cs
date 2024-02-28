b0ae7e9d3378005ad390c9b0218e20fbbc80823a
Miguel5858 <jmti4884@gmail.com>
2024-02-28 11:51:39 -0500

    public class ApplicationDbContext : DbContext     {         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)         {
            }
    
            public DbSet<Cliente> Clientes { get; set; } = null!;
    
        }

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,12 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MaestroDetalle_CRUD.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace MaestroDetalle_CRUD.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; } = null!;
         
     }
 }
