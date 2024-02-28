d4cf09096395760e60a6206291c42ab9a2c0e0bb
Miguel5858 <jmti4884@gmail.com>
2024-02-28 15:27:32 -0500

    listo

--- obj/Debug/net8.0/MaestroDetalle_CRUD.AssemblyInfo.cs
+++ obj/Debug/net8.0/MaestroDetalle_CRUD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaestroDetalle_CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c35eba98c0e6983283f2bf20948b86b587f955")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f2baf8b5c78fb609b2ddbeeb27923d663c3411")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaestroDetalle_CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaestroDetalle_CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
