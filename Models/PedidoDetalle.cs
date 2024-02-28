b920cce9498ee051d79756d014edc53c8408eeb0
Miguel5858 <jmti4884@gmail.com>
2024-02-28 10:50:17 -0500

    public class PedidoDetalle     {         [Key]         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
            public int PedidoDetalleId { get; set; }
            [Required]
            public int Cantidad { get; set; }
    
            [Required]
            public int PedidoId { get; set; }
            public Pedido Pedido { get; set; } = null!;
    
        }

--- Models/PedidoDetalle.cs
+++ Models/PedidoDetalle.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,16 @@ namespace MaestroDetalle_CRUD.Models
 {
     public class PedidoDetalle
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int PedidoDetalleId { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+
+        [Required]
+        public int PedidoId { get; set; }
+        public Pedido Pedido { get; set; } = null!;
         
     }
 }
