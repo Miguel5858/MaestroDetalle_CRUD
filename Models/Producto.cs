eae76e49870a49bcdcb5b804dd846b35c817c63b
Miguel5858 <jmti4884@gmail.com>
2024-02-27 22:06:50 -0500

    using System.ComponentModel.DataAnnotations;
    namespace MaestroDetalle_CRUD.Models
    {
        public class Producto
        {
            public int ProductoId { get; set; }
            [Required]
            public string Nombre { get; set; }=null!;
            [Required]
            public decimal Precio { get; set; }
        }
    }

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,11 +1,15 @@
 
 
+using System.ComponentModel.DataAnnotations;
+
 namespace MaestroDetalle_CRUD.Models
 {
     public class Producto
     {
         public int ProductoId { get; set; }
+        [Required]
         public string Nombre { get; set; }=null!;
+        [Required]
         public decimal Precio { get; set; }
     }
 }
