4d8e7c0fb6c87f96501eaf27f400155e91101cf8
Miguel5858 <jmti4884@gmail.com>
2024-02-27 21:59:46 -0500

    public int ClienteId { get; set;}       [Required]       public string Nombre { get; set; }=null!;     }

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,9 @@ namespace MaestroDetalle_CRUD.Models
 {
     public class Cliente
     {
-        
+
+      public int ClienteId { get; set;}  
+      [Required]
+      public string Nombre { get; set; }=null!;
     }
 }
