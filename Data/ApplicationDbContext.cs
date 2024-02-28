061ba2c0f9cd21b189208b6d8b32d6bf0c128cc5
Miguel5858 <jmti4884@gmail.com>
2024-02-28 11:42:05 -0500

    using System; using System.Collections.Generic; using System.Linq; using System.Threading.Tasks; using Microsoft.EntityFrameworkCore;
    namespace MaestroDetalle_CRUD.Data
    {
        public class ApplicationDbContext:DbContext
        {
    
        }
    }

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MaestroDetalle_CRUD.Data
 {
-    public class ApplicationDbContext:DataContext
+    public class ApplicationDbContext:DbContext
     {
         
     }
