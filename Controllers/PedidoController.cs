48de9ed6cb4d8dc5904d26ddafad8581862ee57b
Miguel5858 <jmti4884@gmail.com>
2024-02-28 21:27:28 -0500

    using System; using System.Collections.Generic; using System.Diagnostics; using System.Linq; using System.Threading.Tasks; using MaestroDetalle_CRUD.Data; using Microsoft.AspNetCore.Mvc; using Microsoft.Extensions.Logging;
    namespace MaestroDetalle_CRUD.Controllers
    {
    
        public class PedidoController : Controller
        {
            private readonly ApplicationDbContext _context;
    
            public PedidoController(ApplicationDbContext context)
           {
               _context = context;
           }
    
        }
    }

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using MaestroDetalle_CRUD.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,6 +12,13 @@ namespace MaestroDetalle_CRUD.Controllers
 
     public class PedidoController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public PedidoController(ApplicationDbContext context)
+       {
+           _context = context;
+       }
+  
   
     }
 }
