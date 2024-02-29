f9dbf98680ad8ce8c3494bb43bb1e167f3ad0dee
Miguel5858 <jmti4884@gmail.com>
2024-02-28 21:43:00 -0500

    public async Task<ActionResult> Create()        {         ViewBag.Clientes = await _context.Clientes.ToListAsync();         ViewBag.Productos = await _context.Productos.ToListAsync();         return View();        }

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MaestroDetalle_CRUD.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace MaestroDetalle_CRUD.Controllers
@@ -17,6 +18,21 @@ namespace MaestroDetalle_CRUD.Controllers
         public PedidoController(ApplicationDbContext context)
        {
            _context = context;
+       }
+       public async Task<ActionResult> Index()
+       {
+        var pedidos= await _context.Pedidos
+        .Include(p=>p.Cliente)
+        .ToListAsync();
+        
+        return View(pedidos);
+       }
+
+         public async Task<ActionResult> Create()
+       {
+        ViewBag.Clientes = await _context.Clientes.ToListAsync();
+        ViewBag.Productos = await _context.Productos.ToListAsync();          
+        return View();
        }
   
   
