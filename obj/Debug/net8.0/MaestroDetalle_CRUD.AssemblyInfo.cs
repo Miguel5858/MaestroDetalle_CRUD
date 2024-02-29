59113016df48e867cbeb7341e9d4b9d909904dd3
Miguel5858 <jmti4884@gmail.com>
2024-02-28 20:56:42 -0500

    crud de productos

--- obj/Debug/net8.0/MaestroDetalle_CRUD.AssemblyInfo.cs
+++ obj/Debug/net8.0/MaestroDetalle_CRUD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaestroDetalle_CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4cf09096395760e60a6206291c42ab9a2c0e0bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13642a1d704b984773929cd11dce665b1210228a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaestroDetalle_CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaestroDetalle_CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
